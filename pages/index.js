fab66b7b1ea6dde97bfe8ad385bd6b2381480a8f
Ben Botvinick <benbotvinick@gmail.com>
2021-01-17 19:17:05 -0500

    Try with wait

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ export default function Index({ date }) {
 }
 
 export async function getServerSideProps({ res }) {
+  await new Promise(res => setTimeout(res, 3000))
+
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=10, stale-while-revalidate=59'
