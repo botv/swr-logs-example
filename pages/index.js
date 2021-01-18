0f927d99e4b741e1b63b2e93e72802297eadc319
Ben Botvinick <benbotvinick@gmail.com>
2021-01-17 23:19:45 -0500

    Remove most properties

--- pages/index.js
+++ pages/index.js
@@ -16,26 +16,26 @@ export async function getServerSideProps({ res }) {
     name: 'Meta Labs',
     active: true,
     domain: 'dev.metalabs.io',
-    stripe_account: 'acct_1GNqJkJDDtxs0Pqr',
-    guild: '528021205538570250',
-    branding: {
-      logo: 'https://i.imgur.com/1cAkawA.png',
-      background: 'https://i.imgur.com/LWV31Gc.jpg',
-      primary_color: null,
-      secondary_color: null
-    },
-    terms_of_service_url: 'https://metalabs.io/terms.html',
-    google_analytics_tracking_id: '',
-    google_tag_manager_container_id: '',
-    privacy_policy_url: 'https://metalabs.io/privacy.html',
-    website_url: 'https://metalabs.io',
-    support_email: 'support@metalabs.io',
-    name_placeholder: 'Lebron James',
-    email_placeholder: 'lebron@gmail.com',
-    settings: { payments: { cancel_action: 'remove_all_roles' } },
-    id: 'byiGC2l_6'
+    // stripe_account: 'acct_1GNqJkJDDtxs0Pqr',
+    // guild: '528021205538570250',
+    // branding: {
+    //   logo: 'https://i.imgur.com/1cAkawA.png',
+    //   background: 'https://i.imgur.com/LWV31Gc.jpg',
+    //   primary_color: null,
+    //   secondary_color: null
+    // },
+    // terms_of_service_url: 'https://metalabs.io/terms.html',
+    // google_analytics_tracking_id: '',
+    // google_tag_manager_container_id: '',
+    // privacy_policy_url: 'https://metalabs.io/privacy.html',
+    // website_url: 'https://metalabs.io',
+    // support_email: 'support@metalabs.io',
+    // name_placeholder: 'Lebron James',
+    // email_placeholder: 'lebron@gmail.com',
+    // settings: { payments: { cancel_action: 'remove_all_roles' } },
+    // id: 'byiGC2l_6'
   }
-  
+
   console.log(sizeof(data))
 
   return {
